c07ab87fdc3d6d90f9daec230fcb33009568b4b6
maksonalves <141428273+maksonalves@users.noreply.github.com>
2023-09-29 11:26:34 -0300

    atibidade revisão 2

--- makson.py
+++ makson.py
@@ -1,1 +1,15 @@
-print("ola mundo")
+#Faça um programa para determinar o número de dígitos de
+#um número inteiro positivo informado.
+while True:
+    numero = int(input('Digite um número inteiro positivo: '))
+    numero_digitos = len(str(numero))
+    print(f'o numero {numero}, tem {numero_digitos - 1} digitos')
+    if numero > 0:
+        continue
+    if numero <= 0:
+        print('O número deve ser positivo!')
+        continue
+    if numero == 0:
+        print('o numero é zero')
+
+#Faça um programa para determinar o número de dígitos de'''
